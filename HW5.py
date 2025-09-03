5be9cb6c93d9fca41c77d7e7733f2be9ebb9165b
Coach Mack <jmckellips@wpsok.org>
2025-09-03 12:33:34 -0500

    Made changes to HW5

--- HW5.py
+++ HW5.py
@@ -5,18 +5,18 @@
 
 #1. Create a list with 9 different numbers inside.
 
-#2. Sort the list.
+#2. Sort the list from highest to lowest.
 
 #3. Create an empty list.
 
-#4. Remove the lowest, highest, and median number from the first list.
+#4. Remove the median number from the first list and add it to the second list.
 
-#5. Add them to the empty list.
+#5. Remove the first number from the first list and add it to the second list.
 
 #6. Print both lists.
 
-#7. Add the three numbers in the second list together and print the result.
+#7. Add the two numbers in the second list together and print the result.
 
 #8. Move the number back to the first list (like you did in #4 and #5 but reversed).
 
-#9. Print the first list. to the empty list.
+#9. Sort the first list from lowest to highest and print it.
