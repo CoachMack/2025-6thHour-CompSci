6df2f4768ac5c5c95e317a854a21f2eedd61bb5e
Coach Mack <jmckellips@wpsok.org>
2025-09-08 14:01:07 -0500

    Added HW4 and HW5 review notes

--- HW4.py
+++ HW4.py
@@ -1,25 +1,28 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW4
 
 
 #1. Print Hello World!
-
+print("Hello World!")
 #1. Create a list with 5 strings containing 5 different names in it.
-
+name_list = ["Shane", "Greg", "Aaden", "Devon", "Alaya"]
 #2. Append a new name onto the Name List.
-
+name_list.append("Ethan")
 #3. Print out the 4th name on the list.
-
+print(name_list[3])
 #4. Create a list with 4 different integers in it.
-
+num_list = [375, 4, 68, 32]
 #5. Insert a new integer into the 2nd spot and print the new list.
-
+num_list.insert(1, 6472)
+print(num_list)
 #6. Sort the list from lowest to highest and print the sorted list.
-
+num_list.sort()
+print(num_list)
 #7. Add the 1st three numbers on the sorted list together and print the sum.
-
+first_three_sum = num_list[0] + num_list[1] + num_list[2]
+print(first_three_sum)
 #8. Create a list with two strings, two variables, and too boolean values.
-
+mixed_up_list = ["John", "Pork", 6, 7, True, False]
 #9. Create a print statement that asks the user to input their own index value for the list on #8.
-
+print(mixed_up_list[int(input("Choose between 0 - 5: "))])
--- HW5.py
+++ HW5.py
@@ -1,22 +1,31 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW5
 
 
 #1. Create a list with 9 different numbers inside.
-
+num_list = [0,1,2,3,4,5,6,7,8]
 #2. Sort the list from highest to lowest.
-
+num_list.sort(reverse=True)
 #3. Create an empty list.
-
+emp_list = []
 #4. Remove the median number from the first list and add it to the second list.
+placeholder = num_list[4]
+num_list.pop(4)
+emp_list.append(placeholder)
 
 #5. Remove the first number from the first list and add it to the second list.
-
+emp_list.append(num_list.pop(0))
 #6. Print both lists.
-
+print(num_list)
+print(emp_list)
 #7. Add the two numbers in the second list together and print the result.
-
+emp_list_sum = sum(emp_list)
+print(emp_list_sum)
 #8. Move the number back to the first list (like you did in #4 and #5 but reversed).
-
+num_list.append(emp_list_sum)
+emp_list.pop(0)
+emp_list.pop(0)
 #9. Sort the first list from lowest to highest and print it.
+num_list.sort()
+print(num_list)
