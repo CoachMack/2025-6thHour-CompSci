d66cea73078f704439452f9c790aabe07b50ec4f
Coach Mack <jmckellips@wpsok.org>
2025-10-06 11:38:03 -0500

    Added HW9

---
+++ HW9.py
@@ -0,0 +1,19 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW9
+
+
+#1. Print "Hello World!"
+
+#2. Create a list with three variables that each randomly generate a number between 1 and 100
+
+#3. Print the list.
+
+#4. Create an if statement that determines which of the three numbers is the highest and prints the result.
+
+#5. Tie the result (the largest number) from #4 to a variable called "num".
+
+#6. Create a nested if statement that prints if num is divisible by 2, divisible by 3, both, or neither.
+
+
+
