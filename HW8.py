bea61247600bd7206ff47ea03892d4fb6106578b
Coach Mack <jmckellips@wpsok.org>
2025-10-21 12:21:03 -0500

    Added HW10

--- HW8.py
+++ HW8.py
@@ -1,20 +1,42 @@
-#Name:
-#Class: 5th Hour
+#Name: Coach Mack
+#Class: 6th Hour
 #Assignment: HW8
+import random
 
 #1. Print "Hello World!"
-
+print("Hello World!")
 #2. Create 3 variables that each randomly generate a number between 1 and 10, named A, B, and C.
-
+a = random.randint(1,10)
+b = random.randint(1,10)
+c = random.randint(1,10)
 #3. Print A, B, and C on the same line.
-
+print(a,b,c)
 #4. Make an if statement that prints if variable A is greater than, less than, or equal to 5.
-
+if a > 5:
+    print("a is greater than 5")
+elif a < 5:
+    print("a is less than 5")
+else:
+    print("a is equal to 5")
 #5. Make an if statement that prints if variable B is between 3 and 7, or not.
-
+if b > 3 and b < 7:
+    print("b is between 3 and 7")
+else:
+    print("b is NOT between 3 and 7")
 #6. Make an if statement that prints if variable C is even or odd.
-
+if c % 2 == 0:
+    print("C is even")
+else:
+    print("C is odd")
 #7. Create a variable whose value is 3 + a randomly generated number between 1 and 20
-
+d = 3 + random.randint(1,20)
+print(d)
 #8. Make an if statement that prints if the variable from #7 is greater than, less than, or equal to A + B + C.
+sum_abc = a + b + c
 
+if d > sum_abc:
+    print(f"{d} is greater than {sum_abc}")
+elif d < sum_abc:
+    print(f"{d} is less than {sum_abc}")
+else:
+    print(f"{d} is equal to {sum_abc}")
--- HW9.py
+++ HW9.py
@@ -1,19 +1,36 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW9
-
+import random
 
 #1. Print "Hello World!"
-
+print("Hello World!")
 #2. Create a list with three variables that each randomly generate a number between 1 and 100
-
+rand_list = [random.randint(1,100),random.randint(1,100),random.randint(1,100)]
 #3. Print the list.
-
+print(rand_list)
 #4. Create an if statement that determines which of the three numbers is the highest and prints the result.
-
+if rand_list[0] >= rand_list[1] and rand_list[0] >= rand_list[2]:
+    print(f"{rand_list[0]} is the biggerest")
+    num = rand_list[0]
+elif rand_list[1] >= rand_list[0] and rand_list[1] >= rand_list[2]:
+    print(f"{rand_list[1]} is the biggerest")
+    num = rand_list[1]
+elif rand_list[2] >= rand_list[0] and rand_list[2] >= rand_list[1]:
+    print(f"{rand_list[2]} is the biggerest")
+    num = rand_list[2]
 #5. Tie the result (the largest number) from #4 to a variable called "num".
 
 #6. Create a nested if statement that prints if num is divisible by 2, divisible by 3, both, or neither.
-
+if num % 2 == 0:
+    if num % 3 == 0:
+        print(f"{num} is divisible by 2 and 3")
+    else:
+        print(f"{num} is divisible by 2 but not 3")
+else:
+    if num % 3 == 0:
+        print(f"{num} is divisible by 3 but not 2")
+    else:
+        print(f"{num} is divisible by neither")
 
 
