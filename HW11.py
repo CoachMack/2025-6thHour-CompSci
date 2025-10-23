6adf87c99704c332987a89c4308f173c7a82a842
Coach Mack <jmckellips@wpsok.org>
2025-10-23 11:26:07 -0500

    Added HW11

---
+++ HW11.py
@@ -0,0 +1,6 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW11
+
+#1. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
