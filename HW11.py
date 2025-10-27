5822057136ddd1eaa47e90c8e3fc2626000d0929
Coach Mack <jmckellips@wpsok.org>
2025-10-27 14:03:10 -0500

    Added HW10 and HW11 review notes

--- HW11.py
+++ HW11.py
@@ -1,6 +1,8 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW11
+
+import random
 
 #1. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
 #https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
@@ -20,6 +22,15 @@
 
 #Print "Thank you!" and end the code
 
+temp = random.randint(1,30)
+print(temp)
+
+if temp > 20:
+    print("It's hot!")
+elif temp > 10:
+    print("It's mild!")
+else:
+    print("It's cold!")
 
 
 
