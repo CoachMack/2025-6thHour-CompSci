5c935e2a9b832e2ea992dd349450a6b353ca91bf
Coach Mack <jmckellips@wpsok.org>
2025-10-27 12:31:44 -0500

    Added SC2

---
+++ SC2.py
@@ -0,0 +1,18 @@
+#Name:
+#Class: 6th Hour
+#Assignment: SC2
+
+
+#A local health clinic is looking to add a quick BMI calculator to their website so that their
+#patients can quickly input their height and weight and be given a number as well as their
+#classification. The classifications are as follows:
+
+# - Underweight: Less than 18.5 BMI
+# - Normal Weight: 18.5 to 24.9 BMI
+# - Overweight: 25 to 29.9 BMI
+# - Obese: 30 or more BMI
+
+#It is up to you to figure out the calculation for an accurate BMI reading and tying it to
+#the right classification
+
+#Code Here:
