a39971009b9610d914a7eee4e6501fdb996e4f51
Coach Mack <jmckellips@wpsok.org>
2025-11-10 14:13:03 -0600

    Added HW12 and HW13 review notes

--- HW13.py
+++ HW13.py
@@ -1,5 +1,5 @@
-#Name:
-#Class: 5th Hour
+#Name: Coach Mack
+#Class: 6th Hour
 #Assignment: HW13
 
 #A common exercise in computer science is "FizzBuzz". The rules of
@@ -9,3 +9,51 @@
 #and if it's divisible by both you say "FizzBuzz".
 
 #Create a while loop that follows the rules of the game.
+
+#THIS IS THE BAD METHOD
+
+i = 1
+
+while i <= 100:
+    if i % 3 == 0 and not i % 5 == 0:
+        print("Fizz")
+    if i % 5 == 0 and not i % 3 == 0:
+        print("Buzz")
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    if not i % 3 == 0 and not i % 5 == 0:
+        print(i)
+    i += 1
+
+#This is the good method
+
+i = 1
+
+while i <= 100:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    i += 1
+
+
+#This is the "true ending" method
+
+i = 1
+
+while i <= 1000:
+    output = ""
+    if i % 3 == 0:
+        output += "Fizz"
+    if i % 5 == 0:
+        output += "Buzz"
+    if i % 7 == 0:
+        output += "Fazz"
+    if output == "":
+        output = i
+    print(output)
+    i += 1
