0294e9073550d5f16c3ea110626023e22315ddbb
Coach Mack <jmckellips@wpsok.org>
2025-11-12 12:21:06 -0600

    Added HW14

---
+++ HW14.py
@@ -0,0 +1,31 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW14
+
+
+#1. Create a for loop with variable i that counts down from 5 to 1 and then prints "Hello World!"
+#at the end.
+
+#2. Create a for loop that counts up and prints only even numbers between 1 and 30.
+
+#3. Create a for loop that prints from 1 to 30 and continues (skips the number) if the number is
+#divisible by 3.
+
+#4. Create a for loop that prints 5 different animals from a list.
+
+#5. Create a for loop that spells out a word you input backwards.
+#(HINT: Google "How to reverse a string in Python")
+
+#6. Create a list containing 10 integers of your choice and print the list.
+
+#7. Create two empty variables named evenNumbers and oddNumbers.
+
+#8. Make a loop that counts the number of even and odd numbers in the list, and prints the
+#result after the loop.
+
+#9. Create a variable that asks the user for an integer and an empty integer variable.
+
+#10. Create a loop with a range from 1 to the number the user input. Use the loop to find the
+#factorial of that number and print the result. A factorial of a number is that number multiplied
+#by every number before it until you reach 1. (For example: 5! is 5 x 4 x 3 x 2 x 1 = 120)
+
