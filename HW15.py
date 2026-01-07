26efbadf15364ebe9b537bade29dd15eed772a61
Coach Mack <jmckellips@wpsok.org>
2026-01-07 12:35:39 -0600

    Added HW15

---
+++ HW15.py
@@ -0,0 +1,31 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW15
+
+#1. import the "random" library
+
+#2. print "Hello World!"
+
+#3. Create three variables named a, b, and c, and allow the user to input an integer for each.
+
+#4. Add a and b together, then divide the sum by c. Print the result.
+
+#5. Round the result from #3 up or down, and then determine if it is even or odd.
+
+#6. Create a list of five different random integers between 1 and 10.
+
+#7. Print the 4th number in the list.
+
+#8. Append another integer to the end of the list, also random from 1 to 10.
+
+#9. Sort the list from lowest to highest and then print the 4th number in the list again.
+
+#10. Create a while loop that starts at 1, prints i and then adds i to itself until it is greater than 100.
+
+#11. Create a list containing the names of five other students in the classroom.
+
+#12. Create a for loop that individually prints out the names of each student in the list.
+
+#13. Create a for loop that counts from 1 to 100, but ends early if the number is a multiple of 10.
+
+#14. Free space. Do something creative. :)
