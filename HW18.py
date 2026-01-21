7621218419e58948359fc9b6c8e30833dca9ee87
Coach Mack <jmckellips@wpsok.org>
2026-01-21 13:12:20 -0600

    Added HW18

---
+++ HW18.py
@@ -0,0 +1,22 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW18
+
+
+#1. Import the "random" library and create a def function that prints "Hello World!"
+
+#2. Create two empty integer variables named "heads" and "tails"
+
+#3. Create a def function that flips a coin one hundred times and increments the result in the above variables.
+
+#4. Call the "Hello world" and "Coin Flip" functions here
+
+#5. Print the final result of heads and tails here
+
+#6. Create a list called beanBag and add at least 5 different colored beans to the list as strings.
+
+#7. Create a def function that pulls a random bean out of the beanBag list, prints which bean you pulled, and then removes it from the list.
+
+#8. Create a def function that asks if you want to pull another bean out of the bag and, if yes, repeats the #3 def function
+
+#9. Call the "Bean Pull" function here
