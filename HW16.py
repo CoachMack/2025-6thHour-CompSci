512bdb5c2d39b9e7b0f59a1c4d8a5b8ecb3e1e0d
Coach Mack <jmckellips@wpsok.org>
2026-02-04 14:04:52 -0600

    Added HW16 and HW17 review code

--- HW16.py
+++ HW16.py
@@ -1,21 +1,44 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW16
+import random
+
 
 #1. Create a def function that prints out "Hello World!"
-
+def hello_world():
+    print("Hello world!")
 #2. Create a def function that calculates the average of three numbers (set the 3 numbers as your arguments).
+def average(a,b,c):
+    avg = (a + b + c)/3
+    print(avg)
+
+def average_list(*num_list):
+    avg = sum(num_list)/len(num_list)
+    print(avg)
+
 
 #3. Create a def function with the names of 5 animals as arguments, treats it like a list, and
 #prints the name of the third animal.
+def animals(*animal_list):
+    print(animal_list[2])
 
 #4. Create a def function that loops from 1 to the number put in the argument.
-
+def loop(end):
+    for i in range(1, end + 1):
+        print(i)
 #5. Call all of the functions created in 1 - 4 with relevant arguments.
-
+hello_world()
+average(1,2,3)
+average_list(5,6,7,8)
+animals("Monkey", "Tiger", "Dog", "Cat", "Pangolin", "Fox")
+loop(5)
 #6. Create a variable x that has the value of 2. Print x
-
-#7. Create a def function that multiplies the value of 2 by a random number between 1 and 5.
-
+x = 2
+print(x)
+#7. Create a def function that multiplies the value of x by a random number between 1 and 5.
+def x_times_random():
+    global x
+    x = x * random.randint(1,5)
+x_times_random()
 #8. Print the new value of x.
-
+print(x)
--- HW17.py
+++ HW17.py
@@ -1,10 +1,56 @@
-#Name:
+#Name: Coach Mack
 #Class: 6th Hour
 #Assignment: HW17
+import random
+
 
 #1. Create a def function that plays a single round of rock, paper, scissors where the user inputs
 #1 for rock, 2 for paper, or 3 for scissors and compares it to a random number generated to serve
 #as the "opponent's hand".
+def rps():
+    user_hand = int(input("Press 1 for Rock, 2 for Paper, 3 for Scissors"))
+    oppo_hand = random.randint(1,3)
+    '''
+    if user_hand == 1 and oppo_hand == 2:
+        print("Opponent Picked Paper! You Lose!")
+    elif user_hand == 1 and oppo_hand == 3:
+        print("Opponent Picked Scissors! You Win!")
+    elif user_hand == 2 and oppo_hand == 1:
+        print("Opponent Picked Rock! You Win!")
+    elif user_hand == 2 and oppo_hand == 3:
+        print("Opponent Picked Scissors! You Lose!")
+    elif user_hand == 3 and oppo_hand == 1:
+        print("Opponent Picked Rock! You Lose!")
+    elif user_hand == 3 and oppo_hand == 2:
+        print("Opponent Picked Paper! You Win!")
+    else:
+        print("You both picked the same thing! Tie!")
+    '''
+
+    if user_hand == oppo_hand:
+        print("You both picked the same thing! Tie!")
+    elif user_hand == 1 and oppo_hand == 3:
+        print("Opponent Picked Scissors! You Win!")
+    elif user_hand == 2 and oppo_hand == 1:
+        print("Opponent Picked Rock! You Win!")
+    elif user_hand == 3 and oppo_hand == 2:
+        print("Opponent Picked Paper! You Win!")
+    else:
+        print("You lose!")
+    repeat_game()
 
 #2. Create a def function that prompts the user to input if they want to play another round, and
 #repeats the RPS def function if they do or exits the code if they don't.
+
+def repeat_game():
+    user_repeat = input("Do you want to play again? Y/N").lower()
+
+    if user_repeat == "y":
+        rps()
+    elif user_repeat == "n":
+        print("Thanks for playing!")
+    else:
+        print("Invalid input. Please try again.")
+        repeat_game()
+
+rps()
