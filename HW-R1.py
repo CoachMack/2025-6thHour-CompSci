c759b7e5a988779760ee865bc513c524b618725a
Coach Mack <jmckellips@wpsok.org>
2026-03-23 12:20:38 -0500

    Added Review 1 and 2

---
+++ HW-R1.py
@@ -0,0 +1,25 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW-R1
+
+#1. Print "Hello World!"
+
+#2. Create three variables with strings as values.
+
+#3. Create three variables with booleans as values.
+
+#4. Create three variables with integers as values.
+
+#5. Create a variable that asks for the user's name and prints it.
+
+#6. Add together the three variables from #4 and print the result.
+
+#7. Subtract the second variable from the first from #4 and print the result.
+
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+
+#9. Create a variable that asks the user for a number with a decimal (a float).
+
+#10. Round the variable from #9 to the nearest whole number and print the result.
+
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
---
+++ HW-R2.py
@@ -0,0 +1,24 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW-R2
+
+
+#1. Print "Hello World!"
+
+#2. Create an empty list.
+
+#3. Create a list that contains the names of everyone in the classroom.
+
+#4. Print the list from #3, sort the list, then print the list again.
+
+#5. Append 5 different integers into the empty list from #2 and print the list.
+
+#6. Add together the middle three numbers in the list from #2 and print the result.
+
+#7. Remove the very first number in the list from #2. Print the new first number.
+
+#8. Create a dictionary with three keys with respective values: your name, your grade, and your favorite color.
+
+#9. Using the update function, add a fourth key and value determining your favorite candy.
+
+#10. Print ONLY the values of the dictionary from #8.
