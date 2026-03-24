7ec7ade36b4f335b349e057c1af14b9e1468bb86
Coach Mack <jmckellips@wpsok.org>
2026-03-24 11:58:16 -0500

    Added Review 3

---
+++ HW-R3.py
@@ -0,0 +1,15 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW-R3
+
+
+#1. import random and print "Hello World!"
+
+#2. Create three variables that each randomly generate an integer between 1 and 10, print each number on the same line.
+
+#3. Create a list containing 5 strings listing 5 colors.
+
+#4. Use a function to randomly choose one of the 5 colors from the list and print the result.
+
+#5. Create an if statement that determines which of the three variables from #2 is the lowest.
+
