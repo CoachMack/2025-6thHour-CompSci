11d47ad41506032889f0d1ec2e8f3160038eb872
Coach Mack <jmckellips@wpsok.org>
2026-04-06 12:39:32 -0500

    Added Review 5

---
+++ HW-R5.py
@@ -0,0 +1,12 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW-R5
+
+#1. Fix the "class" comment at the top to "6th Hour"
+
+#2. Create a list of the names of all the students in the classroom.
+
+#3. Create a for loop that prints the names of every student in the list.
+
+#4. Using the "in" operator (hint: Google), create a for loop that only prints
+#the name of a student if the letter "e" is in it.
