992e53cf7e5a90a79fa931782a4d2fbac6781986
Coach Mack <jmckellips@wpsok.org>
2026-04-13 12:09:27 -0500

    Added Review 6

---
+++ HW-R6.py
@@ -0,0 +1,17 @@
+#Name: Coach Mack
+#Class: 6th Hour
+#Assignment: HW-R6
+
+
+#1. Create a def function that prints out "Hello World!". Call the function.
+
+#2. Create a def function that prints your name. Call the function with the name as the argument.
+
+#3. Create a def function that calculates the average of a list. Call the function with the list as the argument.
+
+#4. Call the function from #3 but with a new list of different numbers.
+
+#5. Create a def function that takes two numbers as arguments, x and y. Inside the function, create a for loop
+#with a range of 10. Inside the loop, make z equal the sum of x and y, make x equal y, then y equal z.
+
+#6. Call the function from #5 with the arguments for x and y being 0 and 1.
