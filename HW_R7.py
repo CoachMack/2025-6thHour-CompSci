90e890a9deee17adce29cc45cc714349b90a359d
Coach Mack <jmckellips@wpsok.org>
2026-04-14 11:54:33 -0500

    Added Review 7 and 8

---
+++ HW_R7.py
@@ -0,0 +1,12 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW_R7
+
+
+#1. Create a class containing a def function that inits self and the three attributes: name, grade, color.
+
+#2. Make a def function within the class that adds 1 to the grade attribute to any object called to it.
+#If they are 12th grade, have the code change their grade to "graduated" instead.
+
+#3. Make a def function within the class that offers the user to input/change their favorite color.
+
---
+++ HW_R8.py
@@ -0,0 +1,15 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW_R8
+
+
+#1. Import all of HW_R7 into this assignment using the from/import function.
+
+#2. Create an object of three students in the classroom. Ask for their name, grade, and favorite color as need be.
+
+#3. Print the name of the first student.
+
+#4. Use the def function from HW_R7 to bump the grade level of the second student up by 1. Print the new grade.
+
+#5. Use the def function from HW_R7 to ask the third student to change their favorite color to something else.
+#Print the new color.
