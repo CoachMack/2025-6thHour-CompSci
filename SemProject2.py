7e52b37713e96ee8197652033a6be689b2044ef9
Coach Mack <jmckellips@wpsok.org>
2026-04-20 11:18:47 -0500

    Added 2nd Semester Project

---
+++ SemProject2.py
@@ -0,0 +1,26 @@
+#Name:
+#Class: 6th Hour
+#Assignment: Semester Project 2
+
+#You're all part of a new development team and the big wigs want to see what you are all capable of.
+#They want you to develop whatever you want to develop, but they want you to work together as a team
+#under agile development and document your work under weekly sprints. You will designate a team lead,
+#who is dedicated to putting the code all together and maintaining it, and a scrum master who will
+#document the progression of the project.
+
+#You have until the end of the week before finals week is due to brainstorm an idea together, and then
+#turn that idea into functioning code. The code itself must have at least: 1 dictionary or list,
+#1 loop, 2 if statements (elif and else statements tied to it does not count towards the total, nor do
+#nested if statements),1 variable with a user input, and 1 form of an assignment operator. You are free
+#to add whatever else you feel is necessary to complete your concept. You will be graded based on how
+#those ideas are implemented together and your overall contribution to the team.
+
+#For the student who is designated scrum master, here is a template you can use to help keep your ideas
+#organized: https://docs.google.com/document/d/1lj8xZ0Y5f0yIAYmHzDW40DiCe2NXuIek-VHHGVOzF1o/edit?tab=t.0
+
+#The Teams are as such:
+
+#Team 1: Gregg, Shane, Ethan, Connor
+#Team 2: Ally, Devon, Tristan, Alaya
+
+
